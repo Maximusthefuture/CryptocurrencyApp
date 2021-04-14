c6da22c6b36b963093eff03b500f63066c7ef147
MaximusTheFuture <naturemade@yandex.ru>
2021-04-14 15:17:36 +0300

    Refactor from struct to class Add double NumberFormatters

---
+++ StockSharesApp/Model/Model.swift
@@ -0,0 +1,55 @@
+//
+//  Model.swift
+//  StockSharesApp
+//
+//  Created by Maximus on 25.02.2021.
+//
+
+import Foundation
+import UIKit
+
+class Model: Equatable {
+    
+    let ticker: String
+    let name: String
+    let logo: String?
+    let price: Double
+    let changePrice: Double
+    var isFavourite: Bool
+    private let priceFormatter = NumberFormatter()
+    private let changePriceFormatter = NumberFormatter()
+    
+    init(ticker: String, name: String, logo: String?, price: Double, changePrice: Double, isFavourite: Bool) {
+        self.ticker = ticker
+        self.name = name
+        self.logo = logo
+        self.price = price
+        self.changePrice = changePrice
+        self.isFavourite = isFavourite
+        setup()
+    }
+    
+    static func == (lhs: Model, rhs: Model) -> Bool {
+        return lhs.ticker  == rhs.ticker && lhs.name == rhs.name
+    }
+    
+    func setup() {
+        priceFormatter.usesGroupingSeparator = true
+        priceFormatter.numberStyle = .currency
+        //MARK: TODO use enum??
+        priceFormatter.locale = Locale.init(identifier: "en_US")
+        changePriceFormatter.numberStyle = .decimal
+        changePriceFormatter.maximumFractionDigits = 2
+        changePriceFormatter.numberStyle = .currency
+        changePriceFormatter.locale = Locale.init(identifier: "en_US")
+    }
+    
+    func formattedPrice() -> String? {
+        priceFormatter.string(from: NSNumber(value: price))
+    }
+    
+    func formattedChangePrice() -> String? {
+        changePriceFormatter.string(from: NSNumber(value: changePrice))
+    }
+    
+}

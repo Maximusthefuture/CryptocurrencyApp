3c05612a4f338f7dfff32b6871a6608163c39534
MaximusTheFuture <naturemade@yandex.ru>
2021-04-25 20:10:38 +0300

    Add chip view(search suggestion cell)

---
+++ StockSharesApp/Views/Cell/SearchSuggestionCell.swift
@@ -0,0 +1,47 @@
+//
+//  SearchSuggestionCell.swift
+//  StockSharesApp
+//
+//  Created by Maximus on 02.04.2021.
+//
+
+import UIKit
+
+protocol setTextDeledate {    
+    func setText(string: String) -> String
+}
+
+
+class SearchSuggestionCell: UICollectionViewCell {
+    
+    var label: UILabel = UILabel()
+    var chipView = ChipView()
+
+    override init(frame: CGRect) {
+        super.init(frame:  frame)
+    }
+    
+    func configView() {
+        chipView.frame = CGRect(x: 0, y: 0, width: contentView.frame.width ,height: 40)
+        chipView.label = label
+        contentView.addSubview(chipView)
+    }
+    
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        configView()
+    }
+    
+}
+
+extension String {
+    func widthOfString(font: UIFont) -> CGFloat {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        let size = self.size(withAttributes: fontAttributes)
+        return size.width
+    }
+}

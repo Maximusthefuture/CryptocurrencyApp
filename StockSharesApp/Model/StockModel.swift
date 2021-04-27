8b6af58c451d84da308c8ee8ab8c8c312a6218a8
MaximusTheFuture <naturemade@yandex.ru>
2021-04-27 20:33:30 +0300

    Add pagination with dispatch group.

--- StockSharesApp/Model/StockModel.swift
+++ StockSharesApp/Model/StockModel.swift
@@ -15,14 +15,19 @@ protocol StockModelDelegate: class {
 
 class StockModel {
     
-    //Получить все данные и кинуть их в array
     var stockModelList: [Model] = []
-    var listOfCompanies = [String]()
     var networkManager: NetworkManager?
     var tickerList: [String] = []
+    var array: [Model] = []
+    var group = DispatchGroup()
     var total = 0
-    var isLoading: Bool?
+    var start = 0
+    var startTicker = 0
+    var end = 13
+    var isFetchInProgress = false
     private weak var delegate: StockModelDelegate?
+    var numOfTickers = 13
+    let mineGroupGlobal = DispatchGroup()
     
     init(networkManager: NetworkManager, delegate: StockModelDelegate) {
         self.networkManager = networkManager
@@ -30,31 +35,89 @@ class StockModel {
     }
     
     func moderator(at index: Int) -> Model {
-      return stockModelList[index]
+        return stockModelList[index]
     }
     
+    var currentCount: Int {
+        stockModelList.count
+    }
     
-    func loadQuotes() {
-        networkManager?.loadCompanyPopularIndices { [weak self] result in
-            self?.tickerList = result?.constituents.suffix(10) ?? []
-            self?.total = (self?.tickerList.count)!
-            print("result symbol is: \(result?.symbol ?? "")")
-            for i in self!.tickerList {
-                print("TICKER LIST: \(i)")
-                self?.networkManager!.getDetails(company: i) { [weak self] p, q in
-                    let name = p?.name ?? "HER"
-                    let current = q?.c ?? 0
-                    let logo = p?.logo ?? "NIL"
-                    guard let prevClosePrice = q?.pc else {
-                        return
-                    }
-                    self?.stockModelList.append(Model(ticker: i, name: name, logo: logo, price: current, changePrice: current - prevClosePrice , isFavourite: false))
-                    DispatchQueue.main.async {
-                        self?.delegate?.onFetchCompleted(with: .none)
+    var totalCount: Int {
+        return total
+    }
+    
+    func loadTest() {
+        guard !self.isFetchInProgress else {
+            return
+        }
+        self.isFetchInProgress = true
+        print("In background")
+        self.networkManager?.loadCompanyPopularTest { result in
+            switch result {
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self.isFetchInProgress = false
+                    self.delegate?.onFetchFailed(with: error.reason)
+                }
+            case .success(let response):
+                self.tickerList = Array(response.constituents[self.startTicker...self.end])
+                self.startTicker += 10
+                self.end += 10
+                print("end \(self.end)")
+                print("startTicker: \(self.startTicker)")
+                print("numoftickers: \(self.numOfTickers)")
+                self.isFetchInProgress = false
+                self.total = response.constituents.count
+                
+                self.getDetails(tickerList: self.tickerList) { (result) in
+                    self.stockModelList.append(result[self.start])
+                    self.start += 1
+                    print(self.start)
+                    print("STOCKMODEL \(self.stockModelList.count)")
+                }
+                DispatchQueue.main.async {
+                    if self.stockModelList.count > 29 {
+                        let indexPathToReload = self.calculateIndexPathToReload(from: self.tickerList)
+                        self.delegate?.onFetchCompleted(with: indexPathToReload)
+                    } else {
+                        self.delegate?.onFetchCompleted(with: .none)
                     }
                 }
             }
         }
+        print("STOCKMODEL TICKERLIST: \(tickerList.count)")
+        
+    }
+    
+    func getDetails(tickerList: [String], completion: @escaping ([Model]) -> Void ) {
+        //походу идет двойной запрос нет?
+        for i in tickerList  {
+            mineGroupGlobal.enter()
+            self.networkManager!.getDetails(company: i) { p, q in
+                let name = p?.name ?? ""
+                let current = q?.c ?? 0
+                let logo = p?.logo ?? "NIL"
+                guard let prevClosePrice = q?.pc else {
+                    return
+                }
+                self.array.append(Model(ticker: i, name: name, logo: logo, price: current, changePrice: current - prevClosePrice , isFavourite: false))
+                print("ARRAYCOUNT: \(self.array.count)")
+                self.mineGroupGlobal.leave()
+                self.group.notify(queue: .main) {
+                    completion(self.array)
+                }
+            }
+        }
+    }
+    
+    private func calculateIndexPathToReload(from newModel: [String]) -> [IndexPath] {
+        let startIndex = stockModelList.count - tickerList.count
+        let endIndex = startIndex + newModel.count
+        print("startIndex: \(startIndex)")
+        print("endIndex: \(endIndex)")
+        print("Index moderatorsCount: \(stockModelList.count)")
+        print("Index newmoderatorsCount: \(tickerList.count)")
+        return (startIndex..<endIndex).map { IndexPath(row: $0, section: 0)}
     }
     
     func filterResults(modelList: [Model], text: String) -> [Model] {
@@ -71,6 +134,7 @@ class StockModel {
     
     func addToFavourite(model: Model) {
         //Favourite list?
+        //favouriteList add?
         stockModelList.append(model)
     }
 }

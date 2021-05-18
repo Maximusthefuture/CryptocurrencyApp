26a680bafec9a3e0feebaa98bcb7b9043391acbe
MaximusTheFuture <naturemade@yandex.ru>
2021-05-18 15:41:56 +0300

    Create protocol for network

---
+++ StockSharesApp/Network/DataProvider.swift
@@ -0,0 +1,16 @@
+//
+//  DataProvider.swift
+//  StockSharesApp
+//
+//  Created by Maximus on 31.03.2021.
+//
+
+import Foundation
+
+
+protocol DataProvider: class {
+    func loadPopular<T: Codable, E: Error>(completion withCompletion: @escaping (Result<T, E>) -> Void)
+    func getDetails<T: Codable, U: Codable>(company: String, completion withCompletion: @escaping (T?, U?) -> Void)
+    func getSearchResults<T: Codable>(query: String, completion withCompletion: @escaping (T?) -> Void)
+    
+}

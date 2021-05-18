013a91a65f6aa3ba37a48ef896572cb6da7ae6b6
MaximusTheFuture <naturemade@yandex.ru>
2021-05-18 15:43:47 +0300

    Renamed, now its model from news

--- StockSharesApp/Model/NewsModel.swift
+++ StockSharesApp/Model/NewsModel.swift
@@ -2,35 +2,37 @@
 //  NewsModel.swift
 //  StockSharesApp
 //
-//  Created by Maximus on 14.05.2021.
+//  Created by Maximus on 18.05.2021.
 //
 
 import Foundation
 
-
-struct NewsModel {
-    let item: [News]
-    let language, lastBuildDate: String
+protocol NewsFetchDelegate: class {
+    func fetchCompleted()
+    func fetchFailed(reason: String)
 }
 
-struct News: Codable, Hashable {
-    let newsDescription: String
-    let link: String
-    let pubDate, title: String
+class NewsModel {
+    let newsProvider: NetworkManager?
+    var newsList: [News] = []
+    weak var delegate: NewsFetchDelegate?
     
-    enum CodingKeys: String, CodingKey {
-        case newsDescription = "description"
-        case link, pubDate, title
+    init(newsProvider: NetworkManager, delegate: NewsFetchDelegate) {
+        self.newsProvider = newsProvider
+        self.delegate = delegate
     }
     
-    static func == (lhs: News, rhs: News) -> Bool {
-        lhs.title == rhs.title
+    func fetchNews(symbol: String) {
+        self.newsProvider?.fetchNews { result in
+            switch result {
+            case .success(let response):
+                self.newsList.append(contentsOf: response.item)
+                print("newsListCount: \(self.newsList.count)")
+                self.delegate?.fetchCompleted()
+            case .failure(let error):
+                self.delegate?.fetchFailed(reason: error.reason)
+            }
+        }
     }
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(title)
-        hasher.combine(newsDescription)
-        hasher.combine(pubDate)
-        hasher.combine(link)
-    }
+
 }
